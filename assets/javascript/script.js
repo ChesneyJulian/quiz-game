136bffc93ecf8f94c42726a444912f40f227a048
Chesney Julian <chesney.julian724@gmail.com>
2023-05-08 16:33:43 -0400

    add questions array and function to add questions to quiz section of page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,10 +5,15 @@ var startEl = document.querySelector('#start');
 var quizEl = document.querySelector('#quiz');
 var endEl = document.querySelector('#end');
 
+var listEl = document.createElement("ul"); 
+
+
+
 startBtn.addEventListener('click', function() {
     startEl.style.display = "none";
     quizEl.style.display = null;
     endEl.style.display = "none";
+    currentQuestion();
 });
 answerBtn.addEventListener('click', function() {
     startEl.style.display ="none";
@@ -21,3 +26,27 @@ submitBtn.addEventListener('click', function() {
     quizEl.style.display="none";
     endEl.style.display="none";
 });
+
+var questionsArr = [ 
+    {
+     question: "Who was tricked into opening the Chamber of Secrets?",
+    },
+    { question: "How was Hermione able to take multiple classes at once?"
+},
+    {question: "Where was Harry Potter born?"
+}, 
+    {question: "What is Seversus's patronus charm?"
+},
+    {question: "What is the name of Hagrids pet hound?"
+}, 
+    {question: "What horcrus does Harry find within the Room of Requirement?"}
+
+]
+
+function currentQuestion() {
+   for (var i = 0; i < questionsArr.length; i++) {
+    var questionEl = document.createElement("h2");
+    questionEl.textContent = questionsArr[i].question;
+    quizEl.appendChild(questionEl);
+   }
+}
