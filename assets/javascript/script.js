529b668dde634b7ac7e952df8aaa19755487ab5d
Chesney Julian <chesney.julian724@gmail.com>
2023-05-09 15:02:49 -0400

    added basic quiz elements to html and add text to elements using currentQuestion function;add event listener for all answerbtns but failing to work past first question

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,5 @@
 var startBtn = document.querySelector('#start-btn');
-var answerBtn = document.querySelector('.answer');
+var answerBtn = document.querySelectorAll('.answer');
 var submitBtn = document.querySelector('#submit'); 
 var startEl = document.querySelector('#start');
 var quizEl = document.querySelector('#quiz');
@@ -7,79 +7,60 @@ var endEl = document.querySelector('#end');
 
 
 
-startBtn.addEventListener('click', function() {
+startBtn.addEventListener('click', function() { 
+    currentQuestion([0]);
     startEl.style.display = "none";
     quizEl.style.display = null;
     endEl.style.display = "none";
 });
-answerBtn.addEventListener('click', function() {
-    startEl.style.display ="none";
-    quizEl.style.display ="none";
-    endEl.style.display = null;
-    currentQuestion();
 
-});
 submitBtn.addEventListener('click', function() {
     startEl.style.display =  null;
     quizEl.style.display="none";
     endEl.style.display="none";
 });
 
+function currentQuestion([i]) {
+    
+    var questionsArr = [ 
+        {question: "Who was tricked into opening the Chamber of Secrets?",
+        choices:["hermione", "Ginny", "Neville", "Draco"] },
 
+        {question: "How was Hermione able to take multiple classes at once?", 
+        choices: ["She ran", "Using a looking glass", "Using a Time-Turner", "Using dark magic"]},
 
-var questionsArr = [ 
-    {question: "Who was tricked into opening the Chamber of Secrets?",
-     choices:["hermione", "Ginny", "Neville", "Draco"] },
+        {question: "Where was Harry Potter born?", 
+        choices:["Godric's hollow", "Privet Drive", "The Leaky Cauldron", "Hogwarts"]}, 
 
-    {question: "How was Hermione able to take multiple classes at once?", 
-    choices: ["She ran", "Using a looking glass", "Using a Time-Turner", "Using dark magic"]},
+        {question: "What is Seversus's patronus charm?", 
+        choices:["Beaver", "Stag", "Bear", "Cat"]},
 
-    {question: "Where was Harry Potter born?", 
-    choices:["Godric's hollow", "Privet Drive", "The Leaky Cauldron", "Hogwarts"]}, 
+        {question: "What is the name of Hagrids pet hound?", 
+        choices:["Bruce", "Grawp", "Arragog", "Fang"]}, 
 
-    {question: "What is Seversus's patronus charm?", 
-    choices:["Beaver", "Stag", "Bear", "Cat"]},
+        {question: "What horcrux does Harry find within the Room of Requirement?", 
+        choices:["Helena's Lost Diadem", "Tom Riddle's Diary", "The sword of Gryffindor", "The Mirror of Erised"]}
+    ]
+    var questionEl = document.querySelector(".question");
+    var answerBtn1 = document.querySelector(".btn1");
+    var answerBtn2 = document.querySelector(".btn2");
+    var answerBtn3 = document.querySelector(".btn3");
+    var answerBtn4 = document.querySelector(".btn4");
+    questionEl.style.display = null;
+    
+    questionEl.textContent = questionsArr[i].question;
+    answerBtn1.textContent = questionsArr[i].choices[0];
+    answerBtn2.textContent = questionsArr[i].choices[1];
+    answerBtn3.textContent = questionsArr[i].choices[2];
+    answerBtn4.textContent = questionsArr[i].choices[3];
 
-    {question: "What is the name of Hagrids pet hound?", 
-    choices:["Bruce", "Grawp", "Arragog", "Fang"]}, 
-
-    {question: "What horcrux does Harry find within the Room of Requirement?", 
-    choices:["Helena's Lost Diadem", "Tom Riddle's Diary", "The sword of Gryffindor", "The Mirror of Erised"]}
-]
-// var possibleChoices = {
-//     q1: ,
-//     q2: ,
-//     q3: ,
-//     q4: ,
-//     q5: ,
-//     q6:  
-// }
-
-
- var listEl = document.createElement("ol"); 
- var questionEl = document.createElement("h2");
- var liEl1 = document.createElement("Li");
- var liEl2 = document.createElement("Li");
- var liEl3 = document.createElement("Li");
- var liEl4 = document.createElement("Li");
-
- 
- questionEl.style.display = null;
- listEl.style.display = null;
-
-function  currentQuestion() {
-    questionEl.textContent = questionsArr[0].question;
-    liEl1.textContent = questionsArr[0].choices[0];
-    liEl2.textContent =questionsArr[0].choices[1];
-    liEl3.textContent = questionsArr[0].choices[2];
-    liEl4.textContent = questionsArr[0].choices[3];
-
-
-    quizEl.appendChild(questionEl);
-    questionEl.appendChild(listEl);
-    listEl.appendChild(liEl1);
-    listEl.appendChild(liEl2);
-    listEl.appendChild(liEl3);
-    listEl.appendChild(liEl4);
+   
 }
- 
+   
+ answerBtn.forEach((button) => {
+    button.addEventListener('click', function() {
+        var i = [0];
+        i++;
+        currentQuestion([i]);
+        });
+})
